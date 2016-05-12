2d662ede65a39cd1a500f23fbd6cf8b9e31697bd
Wfuger <wfuger@gmail.com>
2016-05-12 13:24:00 -0600

    finished /api/v1/users/signup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var knex = require('../db');
+const express = require('express');
+const router = express.Router();
+const knex = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
@@ -19,16 +23,33 @@ router.post('/signup', function(req, res, next) {
         .first()
         .then(function (result) {
           if (result.count === "0") {
-            // we're good
+            const saltRounds = 4;
+            const passwordHash = bcrypt.hashSync(req.body.password, saltRounds);
+            knex('users')
+                .insert({
+                    email: req.body.email,
+                    name: req.body.name,
+                    password_hash: passwordHash
+                })
+                .returning('*')
+                .then(function(users){
+                    //const user = users[0];
+                    const user = users[0];
+                    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+                    res.json({
+                        id: user.id,
+                        email: user.email,
+                        name: user.name,
+                        token: token
+                    })
+                })
           } else {
             res.status(422).json({
               errors: ['Email already exists']
             })
           }
-          //res.json(count)
         })
   }
-  //res.send('respond with a resource');
 });
 
 
